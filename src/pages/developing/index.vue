ec25abb2aab4257d4f93c911648ce79062713cff
九条研介 <dinopresent@qq.com>
2018-11-23 21:43:48 +0800

    屏蔽某些页面

---
+++ src/pages/developing/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <img class="dev" mode="widthFix" src="../../assets/developing.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  methods: {},
+
+  created() {},
+  mounted() {}
+};
+</script>
+
+<style scoped lang="less">
+.container {
+  width: 100%;
+  background: #f3f3f3;
+  .dev {
+    width: 100%;
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,7 +10,8 @@
       <div class="index-tab-item icon-scan" @click="bindTab('../scan/main')">
         <img src="../../assets/icon-index-scan.png" alt="">
       </div>
-      <div class="index-tab-item icon-quiz" @click="bindTab('../quiz/main')">
+      <!-- <div class="index-tab-item icon-quiz" @click="bindTab('../quiz/main')"> -->
+      <div class="index-tab-item icon-quiz" @click="bindTab('../developing/main')">
         <img src="../../assets/icon-index-quiz.png" alt="">
       </div>
       <div class="index-tab-item icon-my" @click="bindTab('../my/main')">
@@ -23,13 +24,11 @@
 </template>
 
 <script>
-import { config } from '../../utils/index';
+import { config } from "../../utils/index";
 
 export default {
   data() {
-    return {
-
-    };
+    return {};
   },
 
   components: {},
@@ -37,25 +36,25 @@ export default {
   methods: {
     setStorage(key, val) {
       try {
-        wx.setStorageSync(key,val)
-      } catch(e) {
-        wx.setStorage(key,val)
+        wx.setStorageSync(key, val);
+      } catch (e) {
+        wx.setStorage(key, val);
       }
     },
     getStorage(key) {
       try {
-        wx.getStorageSync(key)
-      } catch(e) {
-        wx.getStorage(key)
+        wx.getStorageSync(key);
+      } catch (e) {
+        wx.getStorage(key);
       }
     },
     bindTab(url) {
       wx.navigateTo({ url: url });
     },
     login(code) {
-      const userInfo = wx.getStorageSync('userInfo');
+      const userInfo = wx.getStorageSync("userInfo");
       wx.request({
-        url: config.base + 'wxlogin/login',
+        url: config.base + "wxlogin/login",
         data: {
           code: code,
           lineId: config.lineId,
@@ -64,31 +63,31 @@ export default {
           city: userInfo.city,
           province: userInfo.province,
           country: userInfo.country
-        }, 
-        method: 'GET',
-        dataType: 'json', 
+        },
+        method: "GET",
+        dataType: "json",
         success: res => {
           // console.log('login',res.data.data)
-          this.setStorage('userCode',res.data.data)
+          this.setStorage("userCode", res.data.data);
         },
         fail: err => {
-          console.log('hasError',err)
+          console.log("hasError", err);
         }
       });
     },
     getSpot() {
-      const self = this
+      const self = this;
       wx.request({
-        url: config.base + 'spot/list', //开发者服务器接口地址",
+        url: config.base + "spot/list", //开发者服务器接口地址",
         data: {
-          lineId:config.lineId
+          lineId: config.lineId
         }, //请求的参数",
-        method: 'GET',
-        dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+        method: "GET",
+        dataType: "json", //如果设为json，会尝试对返回的数据做一次 JSON.parse
         success: res => {
           // console.log(res)
           // self.GLOBAL.spot_list = res.data.data
-          this.setStorage('spotList',res.data.data)
+          this.setStorage("spotList", res.data.data);
         },
         fail: () => {},
         complete: () => {}
@@ -98,11 +97,11 @@ export default {
 
   created() {
     wx.login({
-      success: (res) => {
-        console.log(res)
+      success: res => {
+        console.log(res);
         this.login(res.code);
       }
-    }); 
+    });
   },
   mounted() {
     this.getSpot();
@@ -124,7 +123,7 @@ export default {
     //     console.log(res)
     //     this.login(res.code);
     //   }
-    // }); 
+    // });
     // this.getSpot();
   }
 };
@@ -148,7 +147,7 @@ export default {
     height: 110rpx;
   }
 }
-.index-list-padding{
+.index-list-padding {
   position: absolute;
   bottom: 220rpx;
   left: 0;
@@ -162,7 +161,7 @@ export default {
   left: 0;
   width: 100%;
   background: rgba(0, 0, 0, 0.7);
-  &-box{
+  &-box {
     padding-top: 40rpx;
     height: 180rpx;
     display: flex;
@@ -182,26 +181,26 @@ export default {
       background: #fff;
       margin-bottom: 12rpx;
     }
-    &-title{
+    &-title {
       width: 160rpx;
-      color:#fff;
-      font-size:20rpx;
+      color: #fff;
+      font-size: 20rpx;
       text-align: center;
     }
   }
-  &-close{
+  &-close {
     width: 80rpx;
     height: 80rpx;
     border-radius: 50%;
-    position: fixed;;
+    position: fixed;
     bottom: 180rpx;
     right: 74rpx;
     z-index: 999;
-    background: rgba(0, 0, 0, .7);
+    background: rgba(0, 0, 0, 0.7);
     display: flex;
     align-items: center;
     justify-content: center;
-    &-body{
+    &-body {
       width: 60rpx;
       height: 60rpx;
       border-radius: 50%;
@@ -209,7 +208,7 @@ export default {
       display: flex;
       align-items: center;
       justify-content: center;
-      img{
+      img {
         width: 32rpx;
         height: 32rpx;
         display: block;
